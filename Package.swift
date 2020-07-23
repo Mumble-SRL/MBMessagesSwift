3a095d0f1036b001c54d3a8c5f1ca47611b29dc5
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-07-23 16:36:39 +0200

    Update package.swiftt

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MBMessagesSwift",
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "MBMessagesSwift",
