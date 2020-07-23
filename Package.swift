0a134469455a7f2e9d085901c0dc7ec1de95ccef
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-07-23 15:59:43 +0200

    Fixed mpush repo

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Mumble-SRL/MBurgerSwift.git", from: "1.0.0"),
-        .package(url: "https://github.com/Mumble-SRL/MPushSwift.git", from: "0.2.13")
+        .package(url: "https://github.com/Mumble-SRL/MPush-Swift.git", from: "0.2.13")
     ],
     targets: [
         .target(
