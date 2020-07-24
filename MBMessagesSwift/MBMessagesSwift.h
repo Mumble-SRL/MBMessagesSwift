15dbbdca69be046e54f2004d87a75ec6e4a63972
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-07-24 09:12:35 +0200

    Renamed obj-c header

--- MBMessagesSwift/MBMessagesSwift.h
+++ MBMessagesSwift/MBMessagesSwift.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for MBMessagesSwift.
-FOUNDATION_EXPORT double MBMessagesVersionNumber;
+FOUNDATION_EXPORT double MBMessagesSwiftVersionNumber;
 
 //! Project version string for MBMessagesSwift.
-FOUNDATION_EXPORT const unsigned char MBMessagesVersionString[];
+FOUNDATION_EXPORT const unsigned char MBMessagesSwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MBMessagesSwift/PublicHeader.h>
 
